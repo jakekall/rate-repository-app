57ec74fc363fc15ebabd35aca3b460f51d9b3986
jakekall <37346883+jakekall@users.noreply.github.com>
2024-07-29 22:31:51 +0300

    Add app bar

---
+++ src/components/AppBarTab.jsx
@@ -0,0 +1,18 @@
+import {  StyleSheet, Pressable } from 'react-native';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  tab: {
+    padding: 20
+  }
+});
+
+const AppBarTab = ({ text }) => {
+  return (
+    <Pressable style={styles.tab}>
+      <Text color='white' fontSize='subheading' fontWeight='bold'>{text}</Text>
+    </Pressable>
+  );
+};
+
+export default AppBarTab;
